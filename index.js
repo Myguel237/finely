c9bd2d507c30a681cc8f58b007ed5f7f5017ee95
Myguel237 <myguelbenzoss@gmail.com>
2024-12-14 16:38:33 +0100

    connection to database

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const morgan = require('morgan');
+
 require('dotenv').config();
 require('./libs/dbConnect');
+
 const app = express();
 app.set('views', './views');
 app.set('view engine', 'ejs');
