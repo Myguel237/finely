31f43f1760334234fc1d71594679591e39804773
Myguel237 <myguelbenzoss@gmail.com>
2025-01-18 10:39:22 +0100

    initial

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,27 +1,10 @@
 const express = require('express');
 const router = express.Router();
-
-const { redirectAuthenticated } = require('../libs/middleware');
-
 const {
-  validateSignup,
-  signup,
-  validateLogin,
-  login,
-  logout
-} = require('../controllers/user.controller');
-
-router.get('/', (req, res) => {
-  res.render('pages/index', { title: 'Finly' });
-  });
-  router.get('/login', (req, res) => {
-  res.render('pages/login', {
-  title: 'Sign in',
-  });
-  });
-  router.get('/signup', (req, res) => {
-  res.render('pages/signup', {
-  title: 'Sign up',
-  });
-  });
-  module.exports = router;
+getUser,
+createUser,
+deleteUser,
+} = require('../controllers/user.controller');router.get('/', getUser);
+router.get('/create', createUser);
+router.get('/delete', deleteUser);
+module.exports = router;
